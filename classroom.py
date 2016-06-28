1f5c763efaf04a7f82a3fd7d8e2812870fbd6457
Yunchao Chen <chenyunchao@xiaoyezi.com>
2016-06-28 16:59:35 +0800

    Add check current classroom info function
    Signed-off-by: Yunchao Chen <chenyunchao@xiaoyezi.com>

--- classroom.py
+++ classroom.py
@@ -1,4 +1,5 @@
 import log
+import json
 import urllib2
 import softdog
 
@@ -23,9 +24,22 @@ def get_server_url(api_level):
     return SERVER_URL_LIST[api_level]
 
 
-def get_current_classroom_info():
+def check_request_result(response):
 
-    url = get_server_url(API_LEVEL_DEV) \
+    obj = json.loads(response)
+    message = obj["request_result"]["message"]
+
+    if message == "OK":
+        log.e("Get current classroom info success.")
+    else:
+        log.e("Get current classroom info failed: " + message)
+
+
+def check_get_current_classroom_info():
+
+    api_level = API_LEVEL_DEV
+
+    url = get_server_url(api_level) \
           + "/api/classroom/1.0/schools/" \
           + softdog.read_soft_dog_data_by_key("schoolCode") \
           + "/classrooms/current"
@@ -39,12 +53,10 @@ def get_current_classroom_info():
     response = urllib2.urlopen(request)
 
     if response.getcode() == 200:
-        log.e("Get current classroom info success.")
-        log.d(response.read())
+        log.e("Can communicate with Server: " + get_server_url(api_level))
+        check_request_result(response.read())
     else:
         log.e("Get current classroom info failed, return code: " + response.getcode())
 
-    return response.read()
 
-
-get_current_classroom_info()
+check_get_current_classroom_info()
